32a6d7b89eec7549681c890a60b743f0a9729efa
LuisGilGB <luis.gilgb@gmail.com>
2019-07-29 13:06:11 +0200

    Fixed clubs picUrl

--- src/bdFutbolClubsScraper.js
+++ src/bdFutbolClubsScraper.js
@@ -5,6 +5,8 @@ const axios = require('axios');
 
 const {JSDOM} = jsdom;
 
+const baseUrl = 'https://www.bdfutbol.com/';
+const esSubpath = 'es/';
 const url = 'https://www.bdfutbol.com/es/t/t2018-19.html';
 
 const bdFutbolClubsScraper = page => {
@@ -16,10 +18,11 @@ const bdFutbolClubsScraper = page => {
     const getIdFromHref = href => href.split('/').reverse()[0].split('.')[0].slice(8);
     const getAlias = r => r.querySelector('.aligesq a').innerHTML;
     const getBdFutbolId = r => getIdFromHref(r.querySelector('.aligesq a').href);
-    const getPicUrl = r => r.querySelector('img').src.replace('/em/', '/eg/');
+    const getPicUrl = r => r.querySelectorAll('td')[2].querySelector('img').src.replace('/em/', '/eg/').replace('../../', baseUrl);
 
     rows.filter(isClubRow)[0] && axios.get(rows.filter(isClubRow)[0].querySelector('.aligesq a').href)
-        .then(response => response.text().then(t => console.log(t)));
+        .then(response => response.text().then(t => console.log(t)))
+        .catch(err => console.log('Cascó'));
 
     return rows.filter(isClubRow)
                .map(r => ({
