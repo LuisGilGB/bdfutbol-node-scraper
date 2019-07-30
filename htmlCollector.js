aaa677e2ee1ef85d9d90a3c981bfe9c92bcf6845
LuisGilGB <luis.gilgb@gmail.com>
2019-07-30 09:31:15 +0200

    Improved code quality at htmlSaves check and create routine

--- htmlCollector.js
+++ htmlCollector.js
@@ -5,35 +5,33 @@ const clubsDir = '/clubs';
 const playersDir = '/players';
 const seasonsDir = '/seasons';
 
+const logDirExists = (dirLogAlias = 'Dir') => console.log(`${dirLogAlias} directory already exists, so no need to create it.`);
+const logDirDoesNotExist = (dirLogAlias = 'Dir') => console.log(`${dirLogAlias} directoty doesn't exist, so we create it now.`);
+const logCreatedDir = (dirLogAlias = 'Dir') => console.log(`${dirLogAlias} directoty successfully created.`);
+
+const createChildDir = (dir = '/dir') => fs.mkdirSync(`${htmlSavesDir}${dir}`);
+
+const checkAndCreateChildDir = (dir = '/dir', dirLogAlias = 'Dir') => {
+    if (fs.existsSync(`${htmlSavesDir}${dir}`)) {
+        logDirExists(dirLogAlias);
+    } else {
+        logDirDoesNotExist(dirLogAlias);
+        createChildDir(dir);
+        logCreatedDir(dirLogAlias);
+    }
+}
+
 if (fs.existsSync(htmlSavesDir)) {
-    console.log("HTML saves dir already exists, so no need to create it.");
-    if (fs.existsSync(`${htmlSavesDir}${clubsDir}`)) {
-        console.log("Clubs directory already exists, so no need to create it.");
-    } else {
-        console.log("Clubs directoty doesn't exist, so we create it now.");
-        fs.mkdirSync(`${htmlSavesDir}${clubsDir}`);
-        console.log("Clubs directoty successfully created.");
-    }
-    if (fs.existsSync(`${htmlSavesDir}${playersDir}`)) {
-        console.log("Players directory already exists, so no need to create it.");
-    } else {
-        console.log("Players directoty doesn't exist, so we create it now.");
-        fs.mkdirSync(`${htmlSavesDir}${playersDir}`);
-        console.log("Players directoty successfully created.");
-    }
-    if (fs.existsSync(`${htmlSavesDir}${seasonsDir}`)) {
-        console.log("Seasons directory already exists, so no need to create it.");
-    } else {
-        console.log("Seasons directoty doesn't exist, so we create it now.");
-        fs.mkdirSync(`${htmlSavesDir}${seasonsDir}`);
-        console.log("Seasons directoty successfully created.");
-    }
+    logDirExists('HTML saves');
+    checkAndCreateChildDir(clubsDir, 'Clubs');
+    checkAndCreateChildDir(playersDir, 'Players');
+    checkAndCreateChildDir(seasonsDir, 'Seasons');
 } else {
-    console.log("HTML saves directoty doesn't exist, so we create it now.");
+    logDirDoesNotExist('HTML saves');
     fs.mkdirSync(htmlSavesDir);
-    fs.mkdirSync(`${htmlSavesDir}${clubsDir}`);
-    fs.mkdirSync(`${htmlSavesDir}${playersDir}`);
-    fs.mkdirSync(`${htmlSavesDir}${seasonsDir}`);
+    createChildDir(clubsDir);
+    createChildDir(playersDir);
+    createChildDir(seasonsDir);
     console.log("HTML saves directoty successfully created with all its children directories.");
 }
 
