8ad61dc67c482e782da2e1cf23909e87b4e4cd9d
LuisGilGB <luis.gilgb@gmail.com>
2019-07-31 17:35:34 +0200

    Page collector simplification: now we only save the DOM portion we are interested in

--- src/bdFutbolPagesCollector.js
+++ src/bdFutbolPagesCollector.js
@@ -4,13 +4,14 @@ const path = require('path');
 const jsdom = require('jsdom');
 const consts = require('./consts.js');
 
+const {JSDOM} = jsdom;
 const {
     baseUrl,
     esSubpath
 } = consts;
 
 const collectPage = (url, destDir) => rp(url)
-                                        .then(response => fs.writeFile(destDir, response, () => console.log('Page successfully saved')))
+                                        .then(response => fs.writeFile(destDir, new JSDOM(response).window.document.querySelector('#classific').outerHTML, () => console.log('Page successfully saved')))
                                         .catch(err => console.log(err));
 
 module.exports = collectPage;
