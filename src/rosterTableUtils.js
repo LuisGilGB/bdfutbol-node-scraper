d3f7a81909d1904a0acf6ce3227add2e1426aa2f
Luis Gil Gutiérrez de la Barreda <luis.gilgb@gmail.com>
2020-03-23 13:12:58 +0100

    Scraping games played data too

--- src/rosterTableUtils.js
+++ src/rosterTableUtils.js
@@ -1,7 +1,15 @@
 const {
     BASE_URL,
     POS_MAP,
-    ROSTER_COLUMNS: {PLAYER_NAME_COL}
+    ROSTER_COLUMNS: {
+        PLAYER_NAME_COL,
+        PLAYER_GAMES_PLAYED,
+        PLAYER_GAME_STARTINGS,
+        PLAYER_GAMES_COMPLETED,
+        PLAYER_YELLOW_CARDS,
+        PLAYER_RED_CARDS,
+        PLAYER_GOALS
+    }
 } = require('./consts.js');
 
 const isPlayerRow = (r, i) => ((i > 0 && i < 12) || i > 14) && r.querySelectorAll('td').length > 7;
@@ -13,6 +21,7 @@ const getBdFutbolId = tds => getIdFromHref(tds[PLAYER_NAME_COL].childNodes[0].ch
 const getUrlFromImageIfExists = img => img ? img.src.replace('/m/', '/j/').replace('../../', BASE_URL).replace('.png', '.jpg') : '';
 const getPicUrl = r => getUrlFromImageIfExists(getImage(r));
 const getPosition = r => POS_MAP[Object.keys(POS_MAP).find(k => r.querySelector(`.${k}`))];
+const getGamesPlayed = tds => +(tds[PLAYER_GAMES_PLAYED].childNodes[0].textContent);
 
 const getPlayerDataFromRow = r => {
     const tds = r.querySelectorAll('td');
@@ -21,7 +30,8 @@ const getPlayerDataFromRow = r => {
         alias: getAlias(tds),
         completeName: getCompleteName(tds),
         bdFutbolId: getBdFutbolId(tds),
-        picUrl: getPicUrl(r)
+        picUrl: getPicUrl(r),
+        gamesPlayed: getGamesPlayed(tds)
     }
 }
 
