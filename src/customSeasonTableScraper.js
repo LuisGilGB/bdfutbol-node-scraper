47c07a8abdc48c1b9ef1ff16af8781973146ed9d
Luis Gil Gutiérrez de la Barreda <luis.gilgb@gmail.com>
2020-03-26 20:49:11 +0100

    Clubs mapper for Spanish Football Bot ready

--- src/customSeasonTableScraper.js
+++ src/customSeasonTableScraper.js
@@ -4,9 +4,13 @@ const seasonTableScraper = require('./seasonTableScraping/seasonTableScraper');
 
 const scrapeSeasonTable = (url, outputFile) => new Promise((resolve, reject) => {
     rp(url).then(html => {
-        const clubs = seasonTableScraper(html);
+        const clubs = seasonTableScraper(html).map(c => ({
+            alias: c.alias,
+            id: c.bdFutbolId,
+            picUrl: c.picUrl
+        }));
         const prevClubs = fs.existsSync(outputFile) ? fs.readJsonSync(outputFile) : [];
-        const updatedClubs = [...prevClubs, ...clubs].filter((c,i,a) => i === a.findIndex(c2 => c2.bdFutbolId === c.bdFutbolId));
+        const updatedClubs = [...prevClubs, ...clubs].filter((c,i,a) => i === a.findIndex(c2 => c2.id === c.id));
         fs.writeJson(outputFile, updatedClubs, {spaces: 2}, (err, outputData) => {
             if (err) {
                 reject(err);
